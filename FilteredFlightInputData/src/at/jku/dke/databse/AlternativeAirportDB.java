b62771e56c858c2a5d18a410f4769cea7e2b9437
Victoria <Victoria@DESKTOP-RQ9ODAM>
2016-12-08 15:44:48 +0100

    First try

--- FilteredFlightInputData/src/at/jku/dke/databse/AlternativeAirportDB.java
+++ FilteredFlightInputData/src/at/jku/dke/databse/AlternativeAirportDB.java
@@ -8,6 +8,9 @@ import com.frequentis.semnotam.pr.AlternateAerodromePropertyType;
 public class AlternativeAirportDB {
 	private static List<AlternateAerodromePropertyType> alternativeAirportDB;
 
+	/**
+	 * @return
+	 */
 	public static List<AlternateAerodromePropertyType> get() {
 		if (alternativeAirportDB == null) {
 			return new ArrayList<>();
